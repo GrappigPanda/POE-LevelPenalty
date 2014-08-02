6511788e178449777e907be8be0dc2d3280f6acd
Ian <ilc.ianclark@gmail.com>
2014-08-02 03:17:05 -0500

    The Actual Final Commit. Everything saved to level_penalty.r. Still one bug to fix

--- level_penalty.r
+++ level_penalty.r
@@ -17,19 +17,6 @@ level_penalty <- function(player, map) {
   final
 }
 
-old_df_penalties <- function(levels, map) {
-  future_matrix = c()
-  
-  size = length(poe.df$levels)
-  
-  for(level in poe.df$levels) {
-    for(map in poe.df$maps) {
-      future_matrix = c(future_matrix, as.numeric(level_penalty(level, map)))
-    }
-  }
-  matrix(future_matrix, size, size, byrow = TRUE)
-}
-
 df_penalties <- function(levels, map) {
   future_matrix = c()
   
@@ -42,4 +29,18 @@ df_penalties <- function(levels, map) {
     }
   }
   matrix(future_matrix, size_lev, size_map, byrow = TRUE)
+}
+
+
+create_graph <- function() {    
+  levels <- c(seq(1,100))
+  monsters <- c(seq(1,78))
+  
+  poe.matrix <- df_penalties(levels, monsters)
+  
+  melted <- melt(poe.matrix)
+  colnames(melted) <- c("Player", "Monster", "Penalty")
+  melted[is.na(melted)] <- 1.00 # I desperately need to figure out why this goes to NaN  
+    
+  ggplot(data=melted, aes(x=Player, y=Monster, fill=Penalty)) + geom_tile() + scale_fill_gradient(low="green", high="red")
 }
