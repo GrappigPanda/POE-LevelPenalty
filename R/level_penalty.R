08b2f9fdab07341f8620c5053afd1d109a3b013c
unknown <yfq652@RR161457IP01.UTSARR.NET>
2015-02-18 15:25:18 -0600

    Folder restructuring/house cleaning.

--- R/level_penalty.R
+++ R/level_penalty.R
@@ -1,12 +1,12 @@
-level_penalty <- function(player, monster) {
-  diff = floor(3 + (player/16))
+levelPenalty <- function(player, monster) {
+  diff = floor(3 + (player / 16))
   lower = player - diff
   upper = player + diff 
   penalty = 0
   
-  if(monster >= lower && monster <= upper) { # No experience penalty
+  if (monster >= lower && monster <= upper) { # No experience penalty
     penalty = 0
-  } else if(monster < lower) {
+  } else if (monster < lower) {
     penalty = lower - monster
   } else { # if(monster > upper) {
     penalty = monster - upper
@@ -17,7 +17,7 @@ level_penalty <- function(player, monster) {
   final
 }
 
-df_penalties <- function(levels, monster) {
+penalties <- function(levels, monster) {
   future_matrix = c()
   
   size_lev = length(levels)
@@ -25,7 +25,7 @@ df_penalties <- function(levels, monster) {
   
   for(level in levels) {
     for(mon in monster) {
-      future_matrix = c(future_matrix, as.numeric(level_penalty(level, mon)))
+      future_matrix = c(future_matrix, as.numeric(levelPenalty(level, mon)))
     }
   }
   matrix(future_matrix, size_lev, size_mon, byrow = TRUE)
@@ -40,10 +40,11 @@ create_graph <- function() {
   levels <- c(seq(1,100))
   monsters <- c(seq(1,78))
   
-  poe.matrix <- df_penalties(levels, monsters)
+  poe.matrix <- penalties(levels, monsters)
   
   melted <- melt(poe.matrix)
   colnames(melted) <- c("Player", "Monster", "Penalty")
     
-  ggplot(data=melted, aes(x=Player, y=Monster, fill=Penalty)) + geom_tile() + scale_fill_gradient(low="green", high="red")
+  ggplot(data = melted, aes(x = Player, y = Monster, fill = Penalty)) 
+	+ geom_tile() + scale_fill_gradient(low = "green", high = "red")
 }
